d0b04a66817542c3775b9996b281ba8b54c306f6
Kassy-D16 <58046765+Kassy-D16@users.noreply.github.com>
2019-11-21 10:17:10 -0800

    Create runGet.php

---
+++ runGet.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<style>
+			
+		</style>
+	</head>
+	<body>
+		<?php
+			$name = $_GET["name"];
+			$email = $_GET["email"];
+			echo "Your name is ".$name." and your email is ".$email;
+		?>
+	</body>
+</html>
