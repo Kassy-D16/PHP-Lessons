efba445659e364029503fa02a610812ad0b842db
Kassy-D16 <58046765+Kassy-D16@users.noreply.github.com>
2019-11-21 10:14:53 -0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<style>
+			
+		</style>
+	</head>
+	<body>
+		<?php
+			$firstVar = "Hello World";
+			echo $firstVar;
+			echo "<h1 id='cats'>".$firstVar."</h1>";
+		?>
+	</body>
+</html>
