8b0c279b4e444eeb7194720be43477ad07752715
Kassy-D16 <58046765+Kassy-D16@users.noreply.github.com>
2019-11-22 11:00:22 -0800

    Create superGlobals.php

---
+++ superGlobals.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<style>
+			
+		</style>
+	</head>
+	<body>
+		<?php
+		
+			echo "Hello World - from: ".$_SERVER['SERVER_SOFTWARE'];
+			echo "<h1 id='cats'>".$_SERVER['HTTP_HOST']."</h1>";
+			echo "<h1 id='cats'>".$_SERVER['REQUEST_TIME']."</h1>";
+		?>
+	</body>
+</html>
