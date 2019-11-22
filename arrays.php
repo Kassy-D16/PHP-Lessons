7ba775e1d2335aca3cbf1cb3227620b81df1f992
Kassy-D16 <58046765+Kassy-D16@users.noreply.github.com>
2019-11-22 11:00:45 -0800

    Create arrays.php

---
+++ arrays.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<style>
+			
+		</style>
+	</head>
+	<body>
+		<?php
+			$firstVar = "Hello World";
+			echo $firstVar;
+			echo "<h1 id='cats'>".$firstVar."</h1>";
+			
+			$testArray = array('test1','test2');
+			foreach($testArray as $test){
+				echo $test;
+			}
+		?>
+	</body>
+</html>
