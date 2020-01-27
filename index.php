082135bc90b5b0ae0f2c6fc40192ababdc8ee9d5
Kassy-D16 <58046765+Kassy-D16@users.noreply.github.com>
2020-01-27 11:08:23 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<style>
+			.grid-container{
+				display:grid;
+				grid-template-columns: auto auto;
+				background-color:#2196f3;
+				padding:10px;
+			}
+			.grid-item{
+				background-color: rgba(255, 255, 255, 0.8);
+				border: 1px solid black;
+				text-align:center;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>The Basic Display</h1>
+		
+		<div class="grid-container">
+			<div class="grid-item"><img src="bestMeme.jfif"></div>
+			<div class="grid-item">Best MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest MemeBest Meme</div>
+			<div class="grid-item">3</div>
+			<div class="grid-item">4</div>
+		</div>
+	<body>
+</html>
